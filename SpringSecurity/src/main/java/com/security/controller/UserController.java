e13b127a3e694d5f247dcdb437f7bf1bbb5baef6
Rahulsosa0000 <131368502+Rahulsosa0000@users.noreply.github.com>
2024-12-19 18:53:48 +0530

    Add files via upload

---
+++ SpringSecurity/src/main/java/com/security/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.security.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.model.User;
+import com.security.services.UserService;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+ 
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/")
+	public List<User> getAllUser() {
+		return this.userService.getAllUser();
+
+	}
+	
+	// get single user
+	@GetMapping("/{username}")
+	public User getSingleUser(@PathVariable String username) {
+		return this.userService.getUser(username);
+	}
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getcsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+		
+	}
+	
+	// add user
+	@PostMapping("/")
+	public User addUser(@RequestBody User user) {
+		return this.userService.addUser(user);
+	}
+	
+	
+	
+
+}
